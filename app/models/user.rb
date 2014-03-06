5e6b2d9094fd8f510340c20c26f41b17c26132c8
Chris Jewell <chrisjohnjewell@gmail.com>
2014-03-06 16:44:11 +1100

    Add unfollow! method

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,11 @@ class User < ActiveRecord::Base
 
     def follow!(other_user)
         relationships.create!(followed_id: other_user.id)
-    end        
+    end 
+    
+    def unfollow!(other_user)
+        relationships.find_by(followed_id: other_user.id).destroy
+    end       
 
     private
 
